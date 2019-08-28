335b152c0cb8ef66c93bf46035f15c19cd1e5f5b
supervons <supervons@sina.com>
2019-08-28 16:30:35 +0800

    1，使用 react-native-scrollable-tab-view 为发现页实现分段显示。
    1. Use react-native-scrollable-tab-view to implement segmentation display for the discovery page.

--- src/screens/Explore/index.js
+++ src/screens/Explore/index.js
@@ -4,9 +4,9 @@
  * explore page
  */
 import React, { Component } from 'react';
-import { StatusBar, View } from 'react-native';
-import Theme from '../../styles/theme';
+import { StyleSheet, ScrollView, Text, View } from 'react-native';
 import newsAction from '../../actions/news';
+import ScrollableTabView from 'react-native-scrollable-tab-view';
 
 export default class Explore extends Component {
   constructor(props) {
@@ -21,6 +21,68 @@ export default class Explore extends Component {
   }
 
   render() {
-    return <View style={{ flex: 1, backgroundColor: '#ffffff' }} />;
+    return (
+      <View style={{ flex: 1, backgroundColor: '#ffffff' }}>
+        <ScrollableTabView
+          tabBarActiveTextColor={this.props.screenProps.themeColor}
+          tabBarUnderlineStyle={{
+            backgroundColor: this.props.screenProps.themeColor
+          }}
+          style={{ height: 800 }}
+        >
+          <ScrollView
+            style={{
+              backgroundColor: '#9DD6EB'
+            }}
+            tabLabel="新闻"
+          >
+            <Text style={styles.text}>新闻</Text>
+          </ScrollView>
+          <ScrollView
+            style={{
+              backgroundColor: '#9DD6EB'
+            }}
+            tabLabel="比赛"
+          >
+            <Text style={styles.text}>比赛</Text>
+          </ScrollView>
+          <ScrollView
+            style={{
+              backgroundColor: '#9DD6EB'
+            }}
+            tabLabel="活动"
+          >
+            <Text style={styles.text}>活动</Text>
+          </ScrollView>
+        </ScrollableTabView>
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  wrapper: { height: 150 },
+  slide1: {
+    height: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB'
+  },
+  slide2: {
+    height: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5'
+  },
+  slide3: {
+    height: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9'
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+});
