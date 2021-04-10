8e511292d6a382952e93d1ec2feef21ad68f0199
supervons <supervons@sina.com>
2021-04-10 14:04:00 +0800

    Hook reconstruction introduction and APP page.

--- App.js
+++ App.js
@@ -9,12 +9,9 @@
  * @format
  * @flow
  */
-
-import React, { Component } from "react";
-
-// 导航路由表
+import React, { useEffect } from "react";
 import { HomeStackScreen } from "./src/routers/index";
-import { SafeAreaView, DeviceEventEmitter, AppState } from "react-native";
+import { SafeAreaView, AppState } from "react-native";
 import Loading from "./src/common/loading";
 import { Provider } from "react-redux";
 import configureStore from "./src/common/redux/store/store";
@@ -22,25 +19,13 @@ import { PersistGate } from "redux-persist/integration/react";
 
 // 引入 redux 及 redux-persist 配置后的变量供使用
 const { store, persist } = configureStore();
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      color: "#f4511E",
-    };
-  }
 
-  componentDidMount() {
-    DeviceEventEmitter.addListener("theme_change", (params) => {
-      this.setState({
-        color: params,
-      });
-    });
+export default function App() {
+  useEffect(() => {
     // 监听app状态：后台挂起，杀死或者活动
-    AppState.addEventListener("change", this._handleAppStateChange);
-  }
-
-  _handleAppStateChange = (nextAppState) => {
+    AppState.addEventListener("change", _handleAppStateChange);
+  }, []);
+  function _handleAppStateChange(nextAppState) {
     if (nextAppState == "inactive") {
       console.log("挂起");
     } else if (nextAppState == "active") {
@@ -48,18 +33,15 @@ export default class App extends Component {
     } else {
       console.log("app杀死");
     }
-  };
-
-  render(): * {
-    return (
-      // 外层需 Provider 包裹， PersistGate 中的 loading 需为一个组件，否则在启动页后会有短暂黑屏
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persist}>
-          <HomeStackScreen />
-          <Loading />
-          <SafeAreaView style={{ backgroundColor: "#ffffff" }} />
-        </PersistGate>
-      </Provider>
-    );
   }
+  return (
+    // 外层需 Provider 包裹， PersistGate 中的 loading 需为一个组件，否则在启动页后会有短暂黑屏
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persist}>
+        <HomeStackScreen />
+        <Loading />
+        <SafeAreaView style={{ backgroundColor: "#ffffff" }} />
+      </PersistGate>
+    </Provider>
+  );
 }
--- src/screens/systemIntroduction/index.js
+++ src/screens/systemIntroduction/index.js
@@ -1,4 +1,6 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigation } from "@react-navigation/core";
 import { View, Text, StatusBar } from "react-native";
 import Swiper from "../../components/SwiperComponent";
 import { Button } from "react-native-elements";
@@ -11,56 +13,51 @@ import commonStyles from "../../styles/commonStyles";
  * Created by supervons on 2019/08/21.
  * 系统功能介绍界面，可以用做第一次进入系统的首屏
  * System function introduction page, Can be used as the first screen to enter the system for the first time
+ *
+ * Update by supervons on 2021/04/10
+ * Hook update
  */
-class SystemIntroduction extends Component {
-  static navigationOptions = {
-    header: null,
-  };
+export default function SystemIntroduction(props) {
+  const firstInstall = useSelector(
+    (state) => state.SettingReducer.firstInstall,
+  );
+  const dispatch = useDispatch();
+  const route = new useNavigation();
+  const [introductionMap] = useState([
+    {
+      style: commonStyles.slide1,
+      title: "本项目包含了大量的 RN 组件",
+      enTitle: "This project contains a large number of RN components",
+    },
+    {
+      style: commonStyles.slide2,
+      title: "源码 github 不断更新",
+      enTitle: "Source code github often updated",
+    },
+    {
+      style: commonStyles.slide3,
+      title: "欢迎你的加入与关注",
+      enTitle: "Welcome to join and star",
+      component: (
+        <Button
+          title={"嗯，朕已知晓"}
+          onPress={() => {
+            if (firstInstall) {
+              dispatch({
+                type: FIRST_INSTALL,
+                firstInstall: false,
+              });
+              route.replace("Login");
+            } else {
+              route.pop();
+            }
+          }}
+        />
+      ),
+    },
+  ]);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      introductionMap: [
-        {
-          style: commonStyles.slide1,
-          title: "本项目包含了大量的 RN 组件",
-          enTitle: "This project contains a large number of RN components",
-        },
-        {
-          style: commonStyles.slide2,
-          title: "源码 github 不断更新",
-          enTitle: "Source code github often updated",
-        },
-        {
-          style: commonStyles.slide3,
-          title: "欢迎你的加入与关注",
-          enTitle: "Welcome to join and star",
-          component: (
-            <Button
-              title={"嗯，朕已知晓"}
-              onPress={() => {
-                if (this.props.firstInstall) {
-                  this.props.setFirstInstall(false);
-                  this.props.navigation.replace("Login");
-                } else {
-                  this.props.navigation.pop();
-                }
-              }}
-            />
-          ),
-        },
-      ],
-    };
-  }
-
-  // 组件加载之前就判断是否是第一次启动
-  componentWillMount() {
-    // if (!this.props.firstInstall && !this.props.navigation.state.params) {
-    //   this.props.navigation.replace('Login');
-    // }
-  }
-
-  renderItem(style, title, enTitle, component) {
+  function renderItem(style, title, enTitle, component) {
     return (
       <View style={[style, { flex: 1 }]}>
         <Animatable.View animation="zoomInUp">
@@ -76,44 +73,16 @@ class SystemIntroduction extends Component {
     );
   }
 
-  render() {
-    return (
-      <View style={{ flex: 1, backgroundColor: "#ffffff" }}>
-        <StatusBar hidden={true} barStyle={"light-content"} />
-        <View style={{ flex: 1 }}>
-          <Swiper loop={false} loadMinimal={false}>
-            {this.state.introductionMap.map((item, i) =>
-              this.renderItem(
-                item.style,
-                item.title,
-                item.enTitle,
-                item.component,
-              ),
-            )}
-          </Swiper>
-        </View>
+  return (
+    <View style={{ flex: 1, backgroundColor: "#ffffff" }}>
+      <StatusBar hidden={true} barStyle={"light-content"} />
+      <View style={{ flex: 1 }}>
+        <Swiper loop={false} loadMinimal={false}>
+          {introductionMap.map((item, i) =>
+            renderItem(item.style, item.title, item.enTitle, item.component),
+          )}
+        </Swiper>
       </View>
-    );
-  }
+    </View>
+  );
 }
-
-// 取出 store 中的数据
-const mapStateToProps = (state) => {
-  return {
-    firstInstall: state.SettingReducer.firstInstall,
-  };
-};
-
-// Dispatch 方法
-const mapDispatchToProps = (dispatch) => {
-  return {
-    setFirstInstall: (firstInstall) => {
-      dispatch({ type: FIRST_INSTALL, firstInstall: firstInstall });
-    },
-  };
-};
-
-export default SystemIntroduction = connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(SystemIntroduction);
