016bb5a40e6010788d6f1e5b0c2ea49b0bc925fd
supervons <supervons@sina.com>
2021-05-29 13:59:49 +0800

    add react-native-draggable's demo page.

--- src/screens/home/drag/DraggablePage.js
+++ src/screens/home/drag/DraggablePage.js
@@ -1,6 +1,26 @@
+/**
+ * @desc Draggable demo page.
+ * Usage in https://github.com/tongyy/react-native-draggable.
+ * @author YS.Feng
+ * @date 2021/05/29
+ */
 import React from "react";
 import { View } from "react-native";
+import Draggable from "react-native-draggable";
 
 export default function DraggablePage() {
-  return <View></View>;
+  return (
+    <View>
+      <Draggable
+        x={200}
+        y={300}
+        renderSize={56}
+        renderColor="black"
+        renderText="A"
+        isCircle
+        shouldReverse
+        onShortPressRelease={() => alert("touched!!")}
+      />
+    </View>
+  );
 }
