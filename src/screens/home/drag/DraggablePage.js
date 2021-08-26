e81aa41a301f155ad9f878071ed566b3b8473def
supervons <supervons@sina.com>
2021-08-26 11:14:36 +0800

    Fix android can't draggable, add animatedViewProps.

--- src/screens/home/drag/DraggablePage.js
+++ src/screens/home/drag/DraggablePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Dimensions } from "react-native";
 import Draggable from "react-native-draggable";
 
 /**
@@ -8,12 +8,14 @@ import Draggable from "react-native-draggable";
  * @author YS.Feng
  * @date 2021/05/29
  */
+const { height } = Dimensions.get("window");
 export default function DraggablePage() {
   return (
     <View>
       <Draggable
         x={200}
         y={300}
+        animatedViewProps={{ height: height }}
         renderSize={56}
         renderColor="black"
         renderText="A"
