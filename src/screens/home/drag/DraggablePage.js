cc4a1f44887cd3d9e7fb1c4b21f5b8406e421cab
supervons <supervons@sina.com>
2021-09-03 11:21:22 +0800

    Add animation component:react-native-reanimated and demo; You should cd ios && pod install.

--- src/screens/home/drag/DraggablePage.js
+++ src/screens/home/drag/DraggablePage.js
@@ -1,27 +1,52 @@
 import React from "react";
-import { View, Dimensions } from "react-native";
+import { View, Dimensions, Button } from "react-native";
 import Draggable from "react-native-draggable";
+import Animated, {
+  withSpring,
+  useSharedValue,
+  useAnimatedStyle,
+} from "react-native-reanimated";
 
 /**
- * @desc Draggable demo page.
+ * @desc Draggable and reanimated demo page.
  * Usage in https://github.com/tongyy/react-native-draggable.
  * @author supervons
  * @date 2021/05/29
  */
 const { height } = Dimensions.get("window");
 export default function DraggablePage() {
+  const offset = useSharedValue(0);
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: offset.value * 255 }],
+    };
+  });
   return (
     <View>
-      <Draggable
-        x={200}
-        y={300}
-        animatedViewProps={{ height: height }}
-        renderSize={56}
-        renderColor="black"
-        renderText="A"
-        isCircle
-        shouldReverse
-        onShortPressRelease={() => alert("touched!!")}
+      <Animated.View style={[animatedStyles]}>
+        <Draggable
+          x={200}
+          y={300}
+          animatedViewProps={{ height: height }}
+          renderSize={56}
+          renderColor="black"
+          renderText="A"
+          isCircle
+          shouldReverse
+          onShortPressRelease={() => alert("touched!!")}
+        />
+      </Animated.View>
+      <Button
+        title={"Move The Ball"}
+        onPress={() => {
+          offset.value = withSpring(Math.random());
+        }}
+      />
+      <Button
+        title={"Reset The Ball"}
+        onPress={() => {
+          offset.value = withSpring(0);
+        }}
       />
     </View>
   );
