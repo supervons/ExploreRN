258c0716d7b6eaf3a956b6f3a48d3c12a74309e7
supervons <supervons@sina.com>
2021-11-10 21:16:55 +0800

    [feature register] Add form state info.

--- src/screens/register/index.js
+++ src/screens/register/index.js
@@ -4,7 +4,7 @@
  * Register page
  * Use email code register.
  */
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
 import { Button } from "react-native-elements";
 import LottieView from "lottie-react-native";
@@ -12,6 +12,13 @@ import I18n from "../../common/languages";
 import { SecurityKeyboardInput } from "react-native-supervons-custom-keyboard";
 
 export default function Register(props) {
+  // User register info
+  const [registerInfo, setRegisterInfo] = useState({
+    uId: "",
+    password: "",
+    userEmail: "",
+    code: "",
+  });
   return (
     <View style={{ justifyContent: "center", alignItems: "center" }}>
       <View style={{ height: 100, width: 300, zIndex: 99, marginBottom: -14 }}>
@@ -21,6 +28,7 @@ export default function Register(props) {
         style={styles.userNameStyle}
         placeholder={I18n.t("Register.userName")}
         placeholderTextColor={"#B1B1B2"}
+        onChangeText={uId => setRegisterInfo({ ...registerInfo, uId })}
       />
       <SecurityKeyboardInput
         keyName={"registerPassword"}
@@ -36,6 +44,9 @@ export default function Register(props) {
         keyboardType={"string"}
         placeholder={I18n.t("Register.password")}
         placeholderTextColor={"#B1B1B2"}
+        onChangeText={password =>
+          setRegisterInfo({ ...registerInfo, password })
+        }
       />
       <View
         style={{
@@ -48,20 +59,35 @@ export default function Register(props) {
             style={styles.emailStyle}
             placeholder={I18n.t("Register.email")}
             placeholderTextColor={"#B1B1B2"}
+            onChangeText={userEmail =>
+              setRegisterInfo({ ...registerInfo, userEmail })
+            }
           />
           <TextInput
             style={styles.emailStyle}
             placeholder={I18n.t("Register.emailCode")}
             placeholderTextColor={"#B1B1B2"}
+            onChangeText={code => setRegisterInfo({ ...registerInfo, code })}
           />
         </View>
         <Button
+          disabled={
+            !registerInfo.uId ||
+            !registerInfo.password ||
+            !registerInfo.userEmail
+          }
           buttonStyle={{ width: 80, height: 95 }}
           containerStyle={{ marginTop: 10 }}
           title={I18n.t("Register.send")}
         />
       </View>
       <Button
+        disabled={
+          !registerInfo.uId ||
+          !registerInfo.password ||
+          !registerInfo.userEmail ||
+          !registerInfo.code
+        }
         onPress={() => navigation.push("SuccessView")}
         buttonStyle={{ width: 300 }}
         containerStyle={{ marginTop: 10 }}
