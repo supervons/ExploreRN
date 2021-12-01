0b3e9fba64faeb065e3f7bc06f3b73afe4723446
supervons <supervons@sina.com>
2021-12-02 00:24:42 +0800

    Embedded WebView add back & go function.

--- src/screens/common/EmbeddedWebview.js
+++ src/screens/common/EmbeddedWebview.js
@@ -1,23 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { Button, View, SafeAreaView } from "react-native";
 import WebView from "react-native-webview";
+import AntDesign from "react-native-vector-icons/AntDesign";
 
 export default function EmbeddedWebview(props) {
   useEffect(() => {});
+  const webViewRef = useRef(null);
+  function commonButton(iconName) {
+    return (
+      <AntDesign
+        style={{ marginLeft: iconName === "right" ? 50 : 0 }}
+        onPress={() =>
+          webViewRef.current[iconName === "right" ? "goForward" : "goBack"]()
+        }
+        name={iconName}
+        size={26}
+        color={"#ccc"}
+      />
+    );
+  }
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <WebView
+        ref={webViewRef}
         startInLoadingState={true}
         source={{ uri: props.route.params.url }}
       />
       <View
         style={{
+          marginTop: 10,
           flexDirection: "row",
           justifyContent: "center",
           alignItems: "center",
         }}>
-        <Button style={{ width: 100 }} title={"<"} onPress={() => {}} />
-        <Button style={{ width: 100 }} title={">"} onPress={() => {}} />
+        {commonButton("left")}
+        {commonButton("right")}
       </View>
     </SafeAreaView>
   );
