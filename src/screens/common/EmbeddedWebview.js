c62a39d1be12f349649b0c56e57c07b28e89a2a9
supervons <supervons@sina.com>
2021-12-01 23:54:59 +0800

    Embedded WebView add back & go button.

--- src/screens/common/EmbeddedWebview.js
+++ src/screens/common/EmbeddedWebview.js
@@ -1,14 +1,24 @@
 import React, { useEffect } from "react";
-import { Text, View, StatusBar } from "react-native";
+import { Button, View, SafeAreaView } from "react-native";
 import WebView from "react-native-webview";
 
 export default function EmbeddedWebview(props) {
   useEffect(() => {});
   return (
-    <View style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1 }}>
       <WebView
         startInLoadingState={true}
-        source={{ uri: props.route.params.url }}></WebView>
-    </View>
+        source={{ uri: props.route.params.url }}
+      />
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+        }}>
+        <Button style={{ width: 100 }} title={"<"} onPress={() => {}} />
+        <Button style={{ width: 100 }} title={">"} onPress={() => {}} />
+      </View>
+    </SafeAreaView>
   );
 }
