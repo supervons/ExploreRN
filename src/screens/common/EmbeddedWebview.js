402eb439ffea0a54d465f42280eabe947e7e61ab
supervons <supervons@sina.com>
2021-12-02 09:38:24 +0800

    Dynamic go and back function.

--- src/screens/common/EmbeddedWebview.js
+++ src/screens/common/EmbeddedWebview.js
@@ -1,29 +1,50 @@
-import React, { useEffect, useRef } from "react";
+/**
+ * Created by supervons on 2021/12/02.
+ * 通用 Webview 组件，自带前进返回按钮，并动态判断是否能前进后退
+ * General Webview component, with its own forward and back button, and dynamic judgment whether can forward and back
+ */
+import React, { useState, useRef } from "react";
 import { Button, View, SafeAreaView } from "react-native";
 import WebView from "react-native-webview";
 import AntDesign from "react-native-vector-icons/AntDesign";
 
 export default function EmbeddedWebview(props) {
-  useEffect(() => {});
+  const [canGoBack, setCanGoBack] = useState(false);
+  const [canGoForward, setCanGoForward] = useState(false);
   const webViewRef = useRef(null);
-  function commonButton(iconName) {
+
+  /**
+   * Dynamic button
+   * @param iconName
+   * @param canTouchable
+   * @returns {*}
+   */
+  function commonButton(iconName, canTouchable) {
     return (
       <AntDesign
+        disabled={!canTouchable}
         style={{ marginLeft: iconName === "right" ? 50 : 0 }}
         onPress={() =>
           webViewRef.current[iconName === "right" ? "goForward" : "goBack"]()
         }
         name={iconName}
         size={26}
-        color={"#ccc"}
+        color={canTouchable ? "#000000" : "#E3E3E3"}
       />
     );
   }
+
+  function onNavigationStateChange(params) {
+    setCanGoBack(params.canGoBack);
+    setCanGoForward(params.canGoForward);
+  }
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <WebView
         ref={webViewRef}
         startInLoadingState={true}
+        onNavigationStateChange={onNavigationStateChange}
         source={{ uri: props.route.params.url }}
       />
       <View
@@ -33,8 +54,8 @@ export default function EmbeddedWebview(props) {
           justifyContent: "center",
           alignItems: "center",
         }}>
-        {commonButton("left")}
-        {commonButton("right")}
+        {commonButton("left", canGoBack)}
+        {commonButton("right", canGoForward)}
       </View>
     </SafeAreaView>
   );
