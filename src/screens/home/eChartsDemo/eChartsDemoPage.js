2f8812d7c05b598d832e3b849bf8d4b744fe3820
supervons <supervons@sina.com>
2022-01-21 15:56:58 +0800

    [opt] Add InteractionManager optimization animation.

--- src/screens/home/eChartsDemo/eChartsDemoPage.js
+++ src/screens/home/eChartsDemo/eChartsDemoPage.js
@@ -3,25 +3,33 @@
  * @author supervons
  * @date 2021/01/20
  */
-import React from "react";
-import { ScrollView } from "react-native";
+import React, { useState, useEffect } from "react";
+import { InteractionManager, ScrollView } from "react-native";
 import ChartsComponent from "../../../components/charts/charts";
 import ChartsLiquidFill from "../../../components/charts/chartsLiquidfill";
 import ChartsExtension from "../../../components/charts/chartsExtension";
 import ModuleHeadTitle from "../../../components/ModuleHeadTitle";
 import I18n from "../../../common/languages";
 
-export default class eChartsDemoPage extends React.Component {
-  render() {
-    return (
-      <ScrollView style={{ flex: 1 }}>
-        <ModuleHeadTitle title={I18n.t("Home.eCharts.chartsDemo")} />
-        <ChartsComponent />
-        <ModuleHeadTitle title={I18n.t("Home.eCharts.extensionDemo")} />
-        <ChartsExtension />
-        <ModuleHeadTitle title={I18n.t("Home.eCharts.liquidDemo")} />
-        <ChartsLiquidFill />
-      </ScrollView>
-    );
-  }
+export default function eChartsDemoPage() {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    InteractionManager.runAfterInteractions(() => {
+      setShow(true);
+    }, []);
+  }, []);
+  return (
+    <ScrollView style={{ flex: 1 }}>
+      {show && (
+        <>
+          <ModuleHeadTitle title={I18n.t("Home.eCharts.chartsDemo")} />
+          <ChartsComponent />
+          <ModuleHeadTitle title={I18n.t("Home.eCharts.extensionDemo")} />
+          <ChartsExtension />
+          <ModuleHeadTitle title={I18n.t("Home.eCharts.liquidDemo")} />
+          <ChartsLiquidFill />
+        </>
+      )}
+    </ScrollView>
+  );
 }
