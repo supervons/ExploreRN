c97de91fc1c300742903244782f748b5010e40c5
supervons <supervons@sina.com>
2022-04-02 00:04:32 +0800

    [code-optimize] Add Echarts notMerge demo.

--- src/components/charts/charts.js
+++ src/components/charts/charts.js
@@ -67,47 +67,50 @@ export default function ChartsComponent() {
   };
 
   function change() {
-    echartsRef.current.setNewOption({
-      tooltip: {
-        trigger: "axis",
-      },
-      legend: {
-        type: "scroll", // 设置图例翻页
-        orient: "horizontal", // 图例横
-        data: ["邮件营销", "联盟广告", "视频广告", "直接访问", "搜索引擎"],
-      },
-      grid: {
-        left: "3%",
-        right: "4%",
-        bottom: "3%",
-        containLabel: true,
-      },
-      xAxis: {
-        type: "category",
-        boundaryGap: false,
-        data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"],
-      },
-      yAxis: {
-        type: "value",
-        axisLabel: {
-          formatter: "{value} kg",
+    echartsRef.current.setNewOption(
+      {
+        tooltip: {
+          trigger: "axis",
         },
+        legend: {
+          type: "scroll", // 设置图例翻页
+          orient: "horizontal", // 图例横
+          data: ["邮件营销", "联盟广告", "视频广告", "直接访问", "搜索引擎"],
+        },
+        grid: {
+          left: "3%",
+          right: "4%",
+          bottom: "3%",
+          containLabel: true,
+        },
+        xAxis: {
+          type: "category",
+          boundaryGap: false,
+          data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"],
+        },
+        yAxis: {
+          type: "value",
+          axisLabel: {
+            formatter: "{value} kg",
+          },
+        },
+        series: [
+          {
+            name: "邮件营销",
+            type: "line",
+            stack: "总量",
+            data: [120, 132, 101, 134, 90, 230, 210],
+          },
+          {
+            name: "联盟广告",
+            type: "line",
+            stack: "总量",
+            data: [220, 182, 191, 234, 290, 330, 310],
+          },
+        ],
       },
-      series: [
-        {
-          name: "邮件营销",
-          type: "line",
-          stack: "总量",
-          data: [120, 132, 101, 134, 90, 230, 210],
-        },
-        {
-          name: "联盟广告",
-          type: "line",
-          stack: "总量",
-          data: [220, 182, 191, 234, 290, 330, 310],
-        },
-      ],
-    });
+      { notMerge: true },
+    );
   }
 
   return (
