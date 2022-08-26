16b9012cd2e9f6170261cba6b2531963597bf076
supervons <supervons@sina.com>
2022-08-26 16:35:33 +0800

    [RN-069] Remove test file.

--- test.js
+++
@@ -1,6 +0,0 @@
-const t0 = global.performance.now();
-const t1 = global.performance.now();
-console.log(t0); // 1768.625000026077
-console.log(t1); // 1768.6300000059418
-const duration = t1-t0;
-console.log(duration);
