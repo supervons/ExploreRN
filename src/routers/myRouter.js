8fa400d2db6e271c7c0d3658fd2a02d91b2c4553
supervons <supervons@sina.com>
2022-08-30 17:00:48 +0800

    [RN-069] Remove scanner page, need redesign.

--- src/routers/myRouter.js
+++ src/routers/myRouter.js
@@ -66,11 +66,12 @@ const routeInfo = [
     options: { title: I18n.t("Route.changePassword") },
     component: UpdatePassword,
   },
-  {
-    name: "Scanner",
-    options: { title: I18n.t("Route.scanner"), header: () => null },
-    component: Scanner,
-  },
+  // TODO-redesign
+  // {
+  //   name: "Scanner",
+  //   options: { title: I18n.t("Route.scanner"), header: () => null },
+  //   component: Scanner,
+  // },
   {
     name: "ScannerResult",
     options: { title: "扫描结果" },
