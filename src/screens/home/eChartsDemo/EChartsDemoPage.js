9fa78a7948597db060f150fc60bb115cca33ee64
supervons <supervons@sina.com>
2023-02-06 20:57:35 +0800

    [code-improved] Remove unused code.

--- src/screens/home/eChartsDemo/EChartsDemoPage.js
+++ src/screens/home/eChartsDemo/EChartsDemoPage.js
@@ -3,8 +3,8 @@
  * @author supervons
  * @date 2021/01/20
  */
-import React, { useState, useEffect, useRef } from "react";
-import { InteractionManager, Platform, ScrollView, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { InteractionManager, ScrollView, View } from "react-native";
 import ChartsComponent from "../../../components/charts/charts";
 import ChartsLiquidFill from "../../../components/charts/chartsLiquidfill";
 import ChartsExtension from "../../../components/charts/chartsExtension";
@@ -14,29 +14,15 @@ import I18n from "../../../common/languages";
 
 export default function EChartsDemoPage() {
   const [show, setShow] = useState(false);
-  const scrRef = useRef(null);
   useEffect(() => {
     InteractionManager.runAfterInteractions(() => {
       setShow(true);
     }, []);
   }, []);
 
-  /**
-   * Improved map sliding
-   * @param state
-   */
-  const handleStop = state => {
-    if (Platform.OS === "android") {
-      scrRef.current.setNativeProps({ scrollEnabled: state });
-    }
-  };
   return (
-    <View
-      style={{ flex: 1 }}
-      onStartShouldSetResponderCapture={() => {
-        handleStop(true);
-      }}>
-      <ScrollView style={{ flex: 1 }} ref={scrRef}>
+    <View style={{ flex: 1 }}>
+      <ScrollView style={{ flex: 1 }}>
         {show && (
           <>
             <ModuleHeadTitle title={I18n.t("Home.eCharts.chartsDemo")} />
