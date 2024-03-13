fafa25d6995569211b1640eac46ec436c2ed8076
supervons <supervons@sina.com>
2024-03-13 17:12:13 +0800

    Add AppCenter libs, add config json, should register in  https://appcenter.ms/users/

--- ios/AwesomeProject/AppDelegate.mm
+++ ios/AwesomeProject/AppDelegate.mm
@@ -2,6 +2,11 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+// Appcenter https://appcenter.ms/users/
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -10,7 +15,10 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
+  // Appcenter
+  [AppCenterReactNative register];
+  [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+  [AppCenterReactNativeCrashes registerWithAutomaticProcessing];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
