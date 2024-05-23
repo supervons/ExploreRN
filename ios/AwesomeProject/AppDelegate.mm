d5e92468907ef8671bcb0b1cd06a9eb0c94cad01
supervons <supervons@sina.com>
2024-05-23 17:26:03 +0800

    Upgrade to RN 0.74.1, you need update yarn version and exec : 1. yarn install; 2. cd ios && pod install.

--- ios/AwesomeProject/AppDelegate.mm
+++ ios/AwesomeProject/AppDelegate.mm
@@ -24,10 +24,10 @@
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-  return [self getBundleURL];
+  return [self bundleURL];
 }
 
-- (NSURL *)getBundleURL
+- (NSURL *)bundleURL
 {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
